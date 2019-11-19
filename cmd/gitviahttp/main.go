d58f99d51df762d4c29652aa718ef8de67ae7a3f
Nirmal Almara <nirmal.almara@outlook.com>
2019-11-19 12:04:38 +0530

    use dir variable in main.go

--- cmd/gitviahttp/main.go
+++ cmd/gitviahttp/main.go
@@ -39,18 +39,18 @@ func main() {
 	var (
 		isServerMode bool
 		port         string
-		repoDir      string
+		dir          string
 	)
 
 	flag.BoolVar(&isServerMode, "server", false, "Specify true for the server mode else it will run in CLI mode")
 	flag.StringVar(&port, "port", "8080", "Specifying the port where gitviahttp should run")
-	flag.StringVar(&repoDir, "directory", ".", "Specify the directory where your repositories are located")
+	flag.StringVar(&dir, "directory", ".", "Specify the directory where your repositories are located")
 
 	flag.Parse()
 
 	if isServerMode {
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			gitviahttp.Context(w, r, ".")
+			gitviahttp.Context(w, r, dir)
 		})
 		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 	} else {
