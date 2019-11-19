58df393e4b3f31f0a666820e87019265644346c3
Nirmal Almara <nirmal.almara@outlook.com>
2019-11-19 11:50:58 +0530

    remove func main from context.go

--- context.go
+++ context.go
@@ -258,26 +258,3 @@ func Context(w http.ResponseWriter, r *http.Request, dir string) {
 
 	writeHdr(w, http.StatusNotFound, "Not found")
 }
-
-// func main() {
-// 	var (
-// 		isServerMode bool
-// 		port         string
-// 		repoDir      string
-// 	)
-
-// 	flag.BoolVar(&isServerMode, "server", false, "Specify true for the server mode else it will run in CLI mode")
-// 	flag.StringVar(&port, "port", "8080", "Specifying the port where gitviahttp should run")
-// 	flag.StringVar(&repoDir, "directory", ".", "Specify the directory where your repositories are located")
-
-// 	flag.Parse()
-
-// 	if isServerMode {
-// 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-// 			gitviahttp.Context(w, r, ".")
-// 		})
-// 		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-// 	} else {
-// 		fmt.Println("Hello, from CLI :)")
-// 	}
-// }
